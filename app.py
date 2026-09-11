384dec4c22e343d1afa1ce992720f60e67477d3e
Akshita <diddigangabhavani@gmail.com>
2026-09-11 13:59:02 +0530

    final working code

--- app.py
+++ app.py
@@ -1,71 +1,22 @@
 from flask import Flask, render_template, request
 import pickle
 
-
-# -----------------------------------
-# Create Flask Application
-# -----------------------------------
-
 app = Flask(__name__)
 
+model = pickle.load(open('model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
 
-# -----------------------------------
-# Load Trained Model
-# -----------------------------------
-
-with open("model.pkl", "rb") as file:
-    model = pickle.load(file)
-
-
-# -----------------------------------
-# Load TF-IDF Vectorizer
-# -----------------------------------
-
-with open("vectorizer.pkl", "rb") as file:
-    vectorizer = pickle.load(file)
-
-
-# -----------------------------------
-# Home Page
-# -----------------------------------
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
-
-# -----------------------------------
-# Fake News Prediction
-# -----------------------------------
-
-@app.route("/predict", methods=["POST"])
+@app.route('/predict', methods=['POST'])
 def predict():
-
-    news_text = request.form["news"]
-
-
-    # Convert text into TF-IDF
+    news_text = request.form['news']
     news_vector = vectorizer.transform([news_text])
-
-
-    # Make prediction
     prediction = model.predict(news_vector)[0]
 
+    return render_template('index.html', prediction=prediction, news=news_text)
 
-   
-
-    # Send result to website
-    return render_template(
-        "index.html",
-        prediction=prediction,
-        
-        news=news_text
-    )
-
-
-# -----------------------------------
-# Run Application
-# -----------------------------------
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
